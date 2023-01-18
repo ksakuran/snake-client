dd3a6c77a0831efd5dce46ff0187ec95d053db50
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-18 20:45:20 +0000

    moved the connect function to the client.js file, module exported an object, with a key equal to the connect function in ES6 shorthand.

--- play.js
+++ play.js
@@ -1,18 +1,6 @@
 const net = require("net");
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-conn.setEncoding("utf8");
-
-conn.on('data', (data) => {
-  console.log(`Server said: ${data}`)
-});
-
-return conn;
-};
+const {connect} = require("./client");
 
 console.log("Connecting . . .");
+
 connect();
