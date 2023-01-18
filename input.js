d41221764ec8e5724294d541d3a4d8b1f3707168
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-18 21:35:04 +0000

    refactored so that the setupInput function was in a different file, required it to the play file and called it in the play file

--- input.js
+++ input.js
@@ -2,6 +2,16 @@ const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
   }
-}
+};
 
-module.exports = {handleUserInput}
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data" , handleUserInput);
+  return stdin;
+};
+
+
+module.exports = {setupInput}
